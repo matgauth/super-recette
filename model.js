1c8c0086fd41435de5d4e992092e9cdcaaee4385
Mathieu Gauthier <mathieu.gauthier.isen@gmail.com>
2018-02-27 16:17:26 +0100

    Added Hindley-Miner signature

--- model.js
+++ model.js
@@ -1,14 +1,18 @@
+// getItems :: _ -> [Item]
 const getItems = () => JSON.parse(localStorage.getItem('items')) || [];
 
+// setItems :: [Item]
 const setItems = items => {
   localStorage.setItem('items', JSON.stringify(items));
 };
 
+// addItem :: (String, [Item])
 const addItem = (text, items) => {
   const id = items.length;
   setItems([...items, { id, text }]);
 };
 
+// removeItem :: (Int, [Item])
 const removeItem = (id, items) => {
   setItems(items.filter(item => item.id !== id));
 };
